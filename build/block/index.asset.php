f1056cd96c5bb746d0ccef143f5df6e026d69dae
Regina Steinberg <regina.steinberg@fau.de>
2026-06-02 14:16:30 +0200

    Build: add parentPath and refresh assets

--- build/block/index.asset.php
+++ build/block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '3fe1a35d8e4c0e7556df');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '65e08e6b9c355d6b0e11');
